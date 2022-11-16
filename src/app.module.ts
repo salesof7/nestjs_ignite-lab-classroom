f4f610719fbfb57e715e36243cd6a74827514b46
salesof7 <jpsaless2002@gmail.com>
2022-11-16 18:48:35 -0300

    feat: add http module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { HttpModule } from './http/http.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
