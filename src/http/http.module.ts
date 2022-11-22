848dc0168df874c8db89ddc15bc33c8e7dba5469
salesof7 <jpsaless2002@gmail.com>
2022-11-21 22:46:46 -0300

    feat: compose a simple resolver structure for each model

--- src/http/http.module.ts
+++ src/http/http.module.ts
@@ -3,6 +3,9 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { DatabaseModule } from 'src/database/database.module';
 import path from 'node:path';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -11,5 +14,6 @@ import path from 'node:path';
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
+  providers: [CoursesResolver, EnrollmentsResolver, StudentsResolver],
 })
 export class HttpModule {}
