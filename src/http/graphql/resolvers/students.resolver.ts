0a55a0c8d39cc3b122f67266ce606a376dde9dca
salesof7 <jpsaless2002@gmail.com>
2022-11-21 22:59:39 -0300

    feat: create query to list all registered data of each model

--- src/http/graphql/resolvers/students.resolver.ts
+++ src/http/graphql/resolvers/students.resolver.ts
@@ -1,5 +1,16 @@
-import { Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Resolver, Query } from '@nestjs/graphql';
+import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { StudentsService } from 'src/http/services/students.service';
 import { Student } from '../models/student';
 
 @Resolver(() => Student)
-export class StudentsResolver {}
+export class StudentsResolver {
+  constructor(private studentsService: StudentsService) {}
+
+  @Query(() => [Student])
+  @UseGuards(AuthorizationGuard)
+  students() {
+    return this.studentsService.listAllStudents();
+  }
+}
--- src/http/services/students.service.ts
+++ src/http/services/students.service.ts
@@ -4,4 +4,8 @@ import { PrismaService } from 'src/database/prisma/prisma.service';
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
+
+  listAllStudents() {
+    return this.prisma.student.findMany();
+  }
 }
